00c2f1c7579e2417d44eba9d71c7bdbc0f73c0cf
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2023-02-14 19:30:57 +0530

    remove

--- hkm/hkm/custom_app/production_plan/production_plan.js
+++ hkm/hkm/custom_app/production_plan/production_plan.js
@@ -1,11 +1,11 @@
-frappe.ui.form.on('Production Plan', {
-	refresh(frm){
-		alert("......refresh")
-		// frm.add_custom_button(__("Finish"), function() {
-		// 	frappe.model.open_mapped_doc({
-		// 		method: "hkm.hkm.custom_app.production_plan.production_plan.make_stock_entry",
-		// 		frm: frm
-		// 	})
-		// }).addClass("btn-primary");
-	}
-});
+// frappe.ui.form.on('Production Plan', {
+// 	refresh(frm){
+// 		alert("......refresh")
+// 		// frm.add_custom_button(__("Finish"), function() {
+// 		// 	frappe.model.open_mapped_doc({
+// 		// 		method: "hkm.hkm.custom_app.production_plan.production_plan.make_stock_entry",
+// 		// 		frm: frm
+// 		// 	})
+// 		// }).addClass("btn-primary");
+// 	}
+// });
